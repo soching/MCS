a72482edcd3cbc811b4205bf1eca6bc7e4199a34
soching <chunsoching@gmail.com>
2024-10-16 13:27:23 +0700

    page boq working type report boq detail print bar code

---
+++ Group_Task/Group_Task/Controllers/BoqWorkingType.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group_Task.Controllers
+{
+    public class BoqWorkingType : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }   
+        public IActionResult AddNewBoq()
+        {
+            return View();
+        }
+    }
+}
