8883a9d846bb5ba51e250a6c6bd447642ee722ff
chun soching <chun_soching@goldenpark.local>
2024-10-17 11:33:34 +0700

    created material type and item type page

---
+++ Group_Task/Group_Task/Controllers/ItemType.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group_Task.Controllers
+{
+    public class ItemType : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
