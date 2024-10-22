282e7fbbbd73a9af7e27fe1144d46d2ce307fa38
soching <chunsoching@gmail.com>
2024-10-22 08:44:36 +0700

    created building page

---
+++ Group_Task/Group_Task/Controllers/BuildingsController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Group_Task.Models;
+using X.PagedList;
+
+namespace Group_Task.Controllers
+{
+    public class BuildingsController : Controller
+    {
+        private readonly TestDbContext _context;
+
+        public BuildingsController(TestDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Buildings
+        public async Task<IActionResult> Index1()
+        {
+              return _context.Buildings != null ? 
+                          View(await _context.Buildings.ToListAsync()) :
+                          Problem("Entity set 'TestDbContext.Buildings'  is null.");
+        }
+
+        public async Task<IActionResult> Index(string Empsearch, int? page)
+        {
+            ViewData["Getemployeedetails"] = Empsearch;
+            ViewData["Title"] = "Index";
+
+            // Define the base query from sql
+            var empquery = from x in _context.Buildings select x;
+
+            // Apply search filter if there's a search term
+            if (!String.IsNullOrEmpty(Empsearch))
+            {
+                empquery = empquery.Where(x =>
+                    (x.ProjectName != null && x.ProjectName.Contains(Empsearch)) ||
+                    (x.Names != null && x.Names.Contains(Empsearch)) ||
+                    (x.Zones != null && x.Zones.Contains(Empsearch)) ||
+                    (x.CreateBy != null && x.CreateBy.Contains(Empsearch))
+                );
+            }
+
+            // Set the page size
+            int pageSize = 25;
+            int pageNumber = page ?? 1; // If page is null, default to page 1
+
+            // Convert query to paginated list
+            var paginatedList = await empquery.AsNoTracking().ToPagedListAsync(pageNumber, pageSize);
+
+            return View(paginatedList);
+        }
+        // GET: Buildings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Buildings == null)
+            {
+                return NotFound();
+            }
+
+            var building = await _context.Buildings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (building == null)
+            {
+                return NotFound();
+            }
+
+            return View(building);
+        }
+
+        // GET: Buildings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Buildings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProjectName,Names,Zones,Blocks,Descriptions,CreateBy,BlockStatus")] Building building)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(building);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(building);
+        }
+
+        // GET: Buildings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Buildings == null)
+            {
+                return NotFound();
+            }
+
+            var building = await _context.Buildings.FindAsync(id);
+            if (building == null)
+            {
+                return NotFound();
+            }
+            return View(building);
+        }
+
+        // POST: Buildings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProjectName,Names,Zones,Blocks,Descriptions,CreateBy,BlockStatus")] Building building)
+        {
+            if (id != building.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(building);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BuildingExists(building.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(building);
+        }
+
+        // GET: Buildings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Buildings == null)
+            {
+                return NotFound();
+            }
+
+            var building = await _context.Buildings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (building == null)
+            {
+                return NotFound();
+            }
+
+            return View(building);
+        }
+
+        // POST: Buildings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Buildings == null)
+            {
+                return Problem("Entity set 'TestDbContext.Buildings'  is null.");
+            }
+            var building = await _context.Buildings.FindAsync(id);
+            if (building != null)
+            {
+                _context.Buildings.Remove(building);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BuildingExists(int id)
+        {
+          return (_context.Buildings?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
